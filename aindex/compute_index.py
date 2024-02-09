adaff4ac5fe3aa19ff944a8f2c6ddbebcc38dc2a
Aleksey Komissarov <ad3002@gmail.com>
2024-02-09 11:19:07 +0200

    refactor compute index

--- aindex/compute_index.py
+++ aindex/compute_index.py
@@ -6,8 +6,13 @@
 #@contact: ad3002@gmail.com
 
 import argparse
-from PyExp import runner
+import subprocess
 import os
+
+def runner(commands):
+    for command in commands:
+        print(command)
+        subprocess.run(command, shell=True)
 
 '''
 ~/Dropbox/Ariadna/Stirka/bin/R2F.exe pe500.mtDNA.reads pe500.mtDNA fasta
@@ -24,9 +29,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Compute index from jf2 file.')
     parser.add_argument('-j', help='JF2 file if exists (None)', required=True)
     parser.add_argument('-o', help='Output prefix', required=True)
-    parser.add_argument('--lu', help='-L for jellyfish', required=False, default=0)
+    parser.add_argument('--lu', help='-L for jellyfish [0]', required=False, default=0)
     parser.add_argument('-P', help='Threads (12)', required=False, default=60)
     parser.add_argument('-M', help='JF2 memory in Gb (5)', required=False, default=1)
+    parser.add_argument('--path_to_aindex', help='Path to aindex folder inside Tools [~/Dropbox/workspace/PyBioSnippets/Tools/aindex]', required=False, default="~/Dropbox/workspace/PyBioSnippets/Tools/aindex")
 
     args = vars(parser.parse_args())
 
@@ -35,21 +41,25 @@ if __name__ == '__main__':
     jf2_file = args["j"]
     memory = args["M"]
     lu = args["lu"]
+    path_to_aindex = args["path_to_aindex"]
+
+    ### here we expect that jf2 file is created
+    if not os.path.exists(jf2_file):
+        print("JF2 file is missing, please, check jellyfish command")
+        exit(1)
 
     commands = [
-            "jellyfish dump -t -c -L %s -o %s.23.dat %s" % (lu, prefix, jf2_file),
+            f"jellyfish dump -t -c -L {lu} -o {prefix}.23.dat {jf2_file}",
+            f"jellyfish histo -o {prefix}.23.histo {jf2_file}",
         ]
-
+    
     runner.run(commands)
 
 
     commands = [
-        "cut -f1 %s.23.dat > %s.23.kmers" % (prefix, prefix),
-        "~/Dropbox/Stirka/emphf/compute_mphf_seq.exe %s.23.kmers %s.23.pf" % (prefix, prefix),
-        "~/Dropbox/Ariadna/Stirka/bin/LU_index.exe %s.23.dat %s.23.pf %s.23 %s" % (prefix, prefix, prefix, threads),
-        "rm %s.23.dat %s.23.kmers" % (prefix, prefix),
+        f"cut -f1 {prefix}.23.dat > {prefix}.23.kmers",
+        f"{path_to_aindex}/compute_mphf_seq.exe {prefix}.23.kmers {prefix}.23.pf",
+        f"{path_to_aindex}/compute_index.exe {prefix}.23.dat {prefix}.23.pf {prefix}.23 {threads} 0",
+        f"rm {prefix}.23.dat {prefix}.23.jf2",
     ]
     runner.run(commands)
-
-   
-
