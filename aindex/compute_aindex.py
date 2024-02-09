31f3cd937e729e78df2bcc9b59a8263978f6becf
Aleksey Komissarov <ad3002@gmail.com>
2024-02-09 11:58:29 +0000

    after black

--- aindex/compute_aindex.py
+++ aindex/compute_aindex.py
@@ -9,12 +9,14 @@ import argparse
 import os
 import subprocess
 
+
 def runner(commands):
     for command in commands:
         print(command)
         subprocess.run(command, shell=True)
 
-'''
+
+"""
 ~/Dropbox/Ariadna/Stirka/bin/R2F.exe pe500.mtDNA.reads pe500.mtDNA fasta
 jellyfish count -m 23 -C -t 60 -s 20G -o pe500.mtDNA.23.jf2 pe500.mtDNA.fa 
 jellyfish dump -c -t -o pe500.mtDNA.23.dat pe500.mtDNA.23.jf2
@@ -22,26 +24,52 @@ cut -f1 pe500.mtDNA.23.dat > pe500.mtDNA.23.kmers
 ~/Dropbox/Stirka/emphf/compute_mphf_seq.exe pe500.mtDNA.23.kmers pe500.mtDNA.23.pf
 ~/Dropbox/Ariadna/Stirka/bin/LU_index.exe pe500.mtDNA.23.dat pe500.mtDNA.23.pf pe500.mtDNA.23
 ~/Dropbox/Ariadna/Stirka/bin/LU_aindex.exe pe500.mtDNA.reads pe500.mtDNA.23.pf pe500.mtDNA.23 pe500.mtDNA.aindex 60 23 pe500.mtDNA.23.tf.bin 
-'''
+"""
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Compute index.')
-    parser.add_argument('-i', help='Fasta, comma separated fastqs or reads', required=True)
-    parser.add_argument('-j', help='JF2 file if exists [None]', required=False, default=None)
-    parser.add_argument('--index', help='Index prefix if exists [None]', required=False, default=None)
-    parser.add_argument('-t', help='Reads type reads, fasta, fastq, se [fastq]', required=False, default="fastq")
-    parser.add_argument('-o', help='Output prefix', required=True)
-    parser.add_argument('-H', help='Build header file for fasta', required=False, default=False)
-    parser.add_argument('--lu', help='-L for jellyfish [0]', required=False, default=0)
-    parser.add_argument('-s', help='Sort dat file [None]', required=False, default=None)
-    parser.add_argument('--interactive', help='Interactive [None]', required=False, default=None)
-    parser.add_argument('-P', help='Threads [12]', required=False, default=12)
-    parser.add_argument('-M', help='JF2 memory in Gb [5]', required=False, default=5)
-    parser.add_argument('--onlyindex', help='Compute only index [False]', required=False, default=False)
-    parser.add_argument('--unzip', help='Unzip files [False]', required=False, default=False)
-    parser.add_argument('--kmers', help='Make kmers file [False]', required=False, default=False)
-    parser.add_argument('--path_to_aindex', help='Path to aindex folder inside Tools [~/Dropbox/workspace/PyBioSnippets/Tools/aindex]', required=False, default="~/Dropbox/workspace/PyBioSnippets/Tools/aindex")
+    parser = argparse.ArgumentParser(description="Compute index.")
+    parser.add_argument(
+        "-i", help="Fasta, comma separated fastqs or reads", required=True
+    )
+    parser.add_argument(
+        "-j", help="JF2 file if exists [None]", required=False, default=None
+    )
+    parser.add_argument(
+        "--index", help="Index prefix if exists [None]", required=False, default=None
+    )
+    parser.add_argument(
+        "-t",
+        help="Reads type reads, fasta, fastq, se [fastq]",
+        required=False,
+        default="fastq",
+    )
+    parser.add_argument("-o", help="Output prefix", required=True)
+    parser.add_argument(
+        "-H", help="Build header file for fasta", required=False, default=False
+    )
+    parser.add_argument("--lu", help="-L for jellyfish [0]", required=False, default=0)
+    parser.add_argument("-s", help="Sort dat file [None]", required=False, default=None)
+    parser.add_argument(
+        "--interactive", help="Interactive [None]", required=False, default=None
+    )
+    parser.add_argument("-P", help="Threads [12]", required=False, default=12)
+    parser.add_argument("-M", help="JF2 memory in Gb [5]", required=False, default=5)
+    parser.add_argument(
+        "--onlyindex", help="Compute only index [False]", required=False, default=False
+    )
+    parser.add_argument(
+        "--unzip", help="Unzip files [False]", required=False, default=False
+    )
+    parser.add_argument(
+        "--kmers", help="Make kmers file [False]", required=False, default=False
+    )
+    parser.add_argument(
+        "--path_to_aindex",
+        help="Path to aindex folder inside Tools [~/Dropbox/workspace/PyBioSnippets/Tools/aindex]",
+        required=False,
+        default="~/Dropbox/workspace/PyBioSnippets/Tools/aindex",
+    )
 
     args = vars(parser.parse_args())
 
@@ -60,7 +88,9 @@ if __name__ == '__main__':
 
     # Check if input required files exist
     required_files = [reads_file.split(",")]
-    missing_files = [file for file in required_files if file and not os.path.exists(file)]
+    missing_files = [
+        file for file in required_files if file and not os.path.exists(file)
+    ]
 
     if missing_files:
         print("The following files are missing:")
@@ -91,7 +121,8 @@ if __name__ == '__main__':
                 input("Continue?")
         elif reads_type == "fasta" or reads_type == "fastq" or reads_type == "se":
             commands = [
-                f"jellyfish count -m 23 -t %s -s %sG -C -L %s -o %s.23.jf2 %s" % (threads, memory, lu, prefix, reads_file.replace(",", " ")),
+                f"jellyfish count -m 23 -t %s -s %sG -C -L %s -o %s.23.jf2 %s"
+                % (threads, memory, lu, prefix, reads_file.replace(",", " ")),
             ]
             runner.run(commands)
             if interactive:
@@ -110,20 +141,21 @@ if __name__ == '__main__':
         if reads_type == "fasta":
             if not build_header:
                 commands = [
-                    f"python {path_to_aindex}/fasta_to_reads.py -i {reads_file} -o {prefix}.reads" % (reads_file, prefix),   
-                ]                
+                    f"python {path_to_aindex}/fasta_to_reads.py -i {reads_file} -o {prefix}.reads"
+                    % (reads_file, prefix),
+                ]
             else:
                 commands = [
-                    f"python {path_to_aindex}/fasta_to_reads.py -i {reads_file} -o {prefix}.reads -H {prefix}.header",   
-                ]                
+                    f"python {path_to_aindex}/fasta_to_reads.py -i {reads_file} -o {prefix}.reads -H {prefix}.header",
+                ]
         if reads_type == "fastq":
             commands = [
-                f"{path_to_aindex}/V2_converter.exe {reads_file.replace(',', ' ')} fastq {prefix}.reads",   
+                f"{path_to_aindex}/V2_converter.exe {reads_file.replace(',', ' ')} fastq {prefix}.reads",
             ]
 
         if reads_type == "se":
             commands = [
-                f"{path_to_aindex}/V2_converter.exe {reads_file.replace(',', ' ')} - se {prefix}.reads",   
+                f"{path_to_aindex}/V2_converter.exe {reads_file.replace(',', ' ')} - se {prefix}.reads",
             ]
 
         runner.run(commands)
@@ -134,7 +166,7 @@ if __name__ == '__main__':
             exit(1)
 
     if not index_prefix:
-        if lu:    
+        if lu:
             commands = [
                 f"jellyfish dump -t -c -L {lu} -o {prefix}.23.dat {jf2_file}",
             ]
@@ -150,8 +182,7 @@ if __name__ == '__main__':
                 f"sort -k2nr {prefix}.23.dat > {prefix}.23.sdat",
             ]
             runner.run(commands)
-    
-    
+
         commands = [
             f"jellyfish histo -o {prefix}.23.histo {jf2_file}",
             f"cut -f1 {prefix}.23.dat > {prefix}.23.kmers",
--- aindex/compute_index.py
+++ aindex/compute_index.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#@created: 01.01.2017
-#@author: Aleksey Komissarov
-#@contact: ad3002@gmail.com
+# @created: 01.01.2017
+# @author: Aleksey Komissarov
+# @contact: ad3002@gmail.com
 
 import argparse
 import subprocess
 import os
+
 
 def runner(commands):
     for command in commands:
         print(command)
         subprocess.run(command, shell=True)
 
-'''
+
+"""
 ~/Dropbox/Ariadna/Stirka/bin/R2F.exe pe500.mtDNA.reads pe500.mtDNA fasta
 jellyfish count -m 23 -C -t 60 -s 20G -o pe500.mtDNA.23.jf2 pe500.mtDNA.fa 
 jellyfish dump -c -t -o pe500.mtDNA.23.dat pe500.mtDNA.23.jf2
@@ -22,17 +24,22 @@ cut -f1 pe500.mtDNA.23.dat > pe500.mtDNA.23.kmers
 ~/Dropbox/Stirka/emphf/compute_mphf_seq.exe pe500.mtDNA.23.kmers pe500.mtDNA.23.pf
 ~/Dropbox/Ariadna/Stirka/bin/LU_index.exe pe500.mtDNA.23.dat pe500.mtDNA.23.pf pe500.mtDNA.23
 ~/Dropbox/Ariadna/Stirka/bin/LU_aindex.exe pe500.mtDNA.reads pe500.mtDNA.23.pf pe500.mtDNA.23 pe500.mtDNA.aindex 60 23 pe500.mtDNA.23.tf.bin 
-'''
+"""
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Compute index from jf2 file.')
-    parser.add_argument('-j', help='JF2 file if exists (None)', required=True)
-    parser.add_argument('-o', help='Output prefix', required=True)
-    parser.add_argument('--lu', help='-L for jellyfish [0]', required=False, default=0)
-    parser.add_argument('-P', help='Threads (12)', required=False, default=60)
-    parser.add_argument('-M', help='JF2 memory in Gb (5)', required=False, default=1)
-    parser.add_argument('--path_to_aindex', help='Path to aindex folder inside Tools [~/Dropbox/workspace/PyBioSnippets/Tools/aindex]', required=False, default="~/Dropbox/workspace/PyBioSnippets/Tools/aindex")
+    parser = argparse.ArgumentParser(description="Compute index from jf2 file.")
+    parser.add_argument("-j", help="JF2 file if exists (None)", required=True)
+    parser.add_argument("-o", help="Output prefix", required=True)
+    parser.add_argument("--lu", help="-L for jellyfish [0]", required=False, default=0)
+    parser.add_argument("-P", help="Threads (12)", required=False, default=60)
+    parser.add_argument("-M", help="JF2 memory in Gb (5)", required=False, default=1)
+    parser.add_argument(
+        "--path_to_aindex",
+        help="Path to aindex folder inside Tools [~/Dropbox/workspace/PyBioSnippets/Tools/aindex]",
+        required=False,
+        default="~/Dropbox/workspace/PyBioSnippets/Tools/aindex",
+    )
 
     args = vars(parser.parse_args())
 
@@ -49,12 +56,11 @@ if __name__ == '__main__':
         exit(1)
 
     commands = [
-            f"jellyfish dump -t -c -L {lu} -o {prefix}.23.dat {jf2_file}",
-            f"jellyfish histo -o {prefix}.23.histo {jf2_file}",
-        ]
-    
-    runner.run(commands)
+        f"jellyfish dump -t -c -L {lu} -o {prefix}.23.dat {jf2_file}",
+        f"jellyfish histo -o {prefix}.23.histo {jf2_file}",
+    ]
 
+    runner.run(commands)
 
     commands = [
         f"cut -f1 {prefix}.23.dat > {prefix}.23.kmers",
